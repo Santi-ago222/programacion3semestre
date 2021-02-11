4b573f1c364d603ef552cbf46d1118c4d2f6a6bc
santi-ago222 <santiago_677@icloud.com>
2021-02-11 09:03:36 -0500

    taller1

---
+++ Clases2021-1/Talleres2021-1/Taller1.py
@@ -0,0 +1,50 @@
+#operaciones
+NumeroA= 17
+NumeroB= 23
+#suma
+suma=NumeroA+NumeroB
+print("#"*20,"sumando","#"*20)
+print(f"la suma es {suma} dando un resultado exitoso")
+#resta
+resta=NumeroA-NumeroB
+print("#"*20,"restando","#"*20)
+print(f"la resta es {resta} dando un resultado exitoso")
+#multiplicacion 
+multiplicacion=NumeroA*NumeroB
+print("#"*20,"Multiplicando","#"*20)
+print(f"la multiplicacion es {multiplicacion} dando un resultado exitoso")
+#division
+Division=NumeroA/NumeroB
+print("#"*20,"division","#"*20)
+print(f"la division es {Division} dando un resultado exitoso")
+#potenciacion
+Potenciacion=NumeroA**NumeroB
+print("#"*20,"potenciacion","#"*20)
+print(f"la potenciacion es {Potenciacion} dando un resultado exitoso")
+#FALSO-VERDADERO
+pruebaF=False
+pruebaV=True
+#mayorque
+print("#"*20,"Mayor que","#"*20)
+isMayorque= NumeroA > NumeroB
+print(isMayorque)
+#mayorigualque
+print("#"*20,"igual que","#"*20) 
+isMayorIgualque= NumeroA >= NumeroB
+print(isMayorIgualque)
+#menorque
+print("#"*20,"menor que","#"*20) 
+Ismenorque= NumeroA < NumeroB
+print(Ismenorque)
+#Mayor a 50
+print("#"*20,"mayor a","#"*20) 
+Ismayora= NumeroA > 50
+print(Ismayora)
+
+
+
+
+
+
+
+
---
+++ Clases2021-1/clase2.py
@@ -0,0 +1,23 @@
+pruebaT=True
+pruebaF=False
+print(pruebaF)
+print(pruebaT)
+pruebaT=pruebaF
+print(pruebaT)
+edad=17
+peso=64
+estatura= 1.74
+NOMBRE= "santiago bermudez gutierrez"
+#Comprobando si es mayor de edad
+print("#" *15, "Mayor edad", "#"*15)
+isMayorEdad = edad >= 18
+print(isMayorEdad)
+#comprobando si estoy bajo la estructura promedio
+print("#" *15, "Bajo la estatura promedio", "#"*15)
+Ismayorestatura=estatura > 1.70
+print (Ismayorestatura)
+#comprobando si tengo un peso diferente 
+print("#" *15, "peso diferente a", "#"*15)
+isPesoDiferente = peso != 75
+print(isPesoDiferente)
+
