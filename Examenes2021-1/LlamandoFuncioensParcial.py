cb67d4ae9eb81f891e548f9eff1a5f5313234c21
santi-ago222 <santiago_677@icloud.com>
2021-04-08 08:14:13 -0500

    .

---
+++ Examenes2021-1/LlamandoFuncioensParcial.py
@@ -0,0 +1,12 @@
+import Parcial1
+
+Parcial1.lineas
+Parcial1.sumar
+Parcial1.restar
+Parcial1.dividir
+Parcial1.multiplicar
+Parcial1.potenciacion
+Parcial1.calculadora
+Parcial1.mostrarlista
+Parcial1.calcular
+Parcial1.topes
